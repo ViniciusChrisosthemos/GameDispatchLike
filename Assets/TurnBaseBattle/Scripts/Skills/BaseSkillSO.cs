c1282519050a430d6085fad037a6475ff43afddb
ViniciusChrisosthemos <vinicius.teixeira.002@acad.pucrs.br>
2025-11-14 20:33:26 -0300

    comeca skill tree warrior

--- Assets/TurnBaseBattle/Scripts/Skills/BaseSkillSO.cs
+++ Assets/TurnBaseBattle/Scripts/Skills/BaseSkillSO.cs
@@ -1,14 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+
+[Serializable]
+public abstract class SkillLevel
+{
+    [SerializeField] protected string BaseDescription;
+    public abstract string GetDescription();
+}
 
 public abstract class BaseSkillSO : ScriptableObject
 {
     public string Name;
     public string BaseDescription;
     public Sprite Art;
-    public int CountDown;
     public List<DiceValueSO> RequiredDiceValues;
     public List<SkillBonus> Bonus;
     public int TargetAmount;
@@ -44,5 +51,6 @@ public abstract class BaseSkillSO : ScriptableObject
     }
 
     protected abstract string GetInternalDescription();
-    public abstract void ApplySkill(IBattleCharacter user, List<IBattleCharacter> targets);
+    public abstract void ApplySkill(int skillLevel, IBattleCharacter user, List<IBattleCharacter> targets);
+    public abstract List<SkillLevel> GetLevels();
 }
--- Assets/TurnBaseBattle/Scripts/Skills/HealSkillSO.cs
+++ Assets/TurnBaseBattle/Scripts/Skills/HealSkillSO.cs
@@ -1,23 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
+
+[Serializable]
+public class HealSkillLevel : SkillLevel
+{
+    public int BaseValue;
+    public List<SkillBonus> Bonus;
+
+    public override string GetDescription()
+    {
+        return string.Format(BaseDescription, BaseValue);
+    }
+}
 
 [CreateAssetMenu(fileName = "Heal_Skill_", menuName = "ScriptableObjects/TurnBasedBattle/Heal Skill")]
 public class HealSkillSO : BaseSkillSO
 {
     public int HealAmount;
+    public List<HealSkillLevel> Levels;
 
-    public override void ApplySkill(IBattleCharacter user, List<IBattleCharacter> targets)
+    public override void ApplySkill(int skillLevel, IBattleCharacter user, List<IBattleCharacter> targets)
     {
-        var finalHeal = HealAmount;
+        var currentBaseHeal = 0;
+        var currentBonus = new List<SkillBonus>();
 
-        foreach (var bonus in Bonus)
+        foreach (var level in Levels)
         {
-            finalHeal = bonus.Type == BonusType.Flat ? Mathf.RoundToInt(finalHeal) : Mathf.RoundToInt(HealAmount * bonus.BaseValue);
+            currentBaseHeal += level.BaseValue;
+            currentBonus.AddRange(level.Bonus);
+        }
+
+        var finalHeal = currentBaseHeal;
+
+        foreach (var bonus in currentBonus)
+        {
+            finalHeal = bonus.Type == BonusType.Flat ? Mathf.RoundToInt(finalHeal) : Mathf.RoundToInt(currentBaseHeal * bonus.BaseValue);
         }
 
         targets.ForEach(target => target.TakeHeal(finalHeal));
+    }
+
+    public override List<SkillLevel> GetLevels()
+    {
+        return Levels.Select(level => (SkillLevel) level).ToList();
     }
 
     protected override string GetInternalDescription()
