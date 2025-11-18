33070b535c935613a0d87b5ff15f1f80f8b1eb13
ViniciusChrisosthemos <vinicius.teixeira.002@acad.pucrs.br>
2025-11-18 19:17:49 -0300

    resolve bugs de merge

--- Assets/Scripts/View/General/UIItemController.cs
+++ Assets/Scripts/View/General/UIItemController.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class UIItemController : MonoBehaviour
@@ -15,6 +13,18 @@ public abstract class UIItemController : MonoBehaviour
         _onClick = controller;
 
         HandleInit(obj);
+    }
+
+    public void SetCallback(Action<UIItemController> callback)
+    {
+        _onClick = callback;
+    }
+
+    public void SetItem(object item)
+    {
+        _item = item;
+
+        HandleInit(item);
     }
 
     protected abstract void HandleInit(object obj);
